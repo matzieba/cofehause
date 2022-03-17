883d25d6af04c65e176fd9a436a406e044739059
mati <matizieba@gmail.com>
2022-03-17 08:57:06 +0100

    psy_cop

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def load_user(user_id):
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///cofehauses.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
