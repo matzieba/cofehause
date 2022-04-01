44bc35fc38d63e12eb4c96606289b7e797e3466a
Mateusz Zieba <matizieba@gmail.com>
2022-04-01 17:56:22 +0200

    database down

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ def load_user(user_id):
 
 
 ##CONNECT TO DB
-#
-#os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
+#"sqlite:///coffe.db"
+#.replace("://", "ql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///coffe.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+
 db = SQLAlchemy(app)
 
 ##CONFIGURE DB TABLES
