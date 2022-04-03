6470cbcb32c7c25d451d7ec34e3a5a9f092687ce
Mateusz Zieba <matizieba@gmail.com>
2022-04-03 15:35:07 +0200

    database postgres

--- main.py
+++ main.py
@@ -31,6 +31,14 @@ db = SQLAlchemy(app)
 
 ##CONFIGURE DB TABLES
 
+class Users(UserMixin,db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(250), nullable=False, unique=True)
+    password = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    about = db.Column(db.Text)
+
 class CofeHauses(db.Model):
     __tablename__ = "cofe hauses"
     id = db.Column(db.Integer, primary_key=True, unique=True)
@@ -47,16 +55,6 @@ class CofeHauses(db.Model):
 
     user_numb = relationship("Users", foreign_keys = user_id)
     user_nam = relationship("Users", foreign_keys = user_name)
-
-class Users(UserMixin,db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    name = db.Column(db.String(250), nullable=False, unique=True)
-    password = db.Column(db.String(250), nullable=False)
-    email = db.Column(db.String(250), unique=True, nullable=False)
-    about = db.Column(db.Text)
-
-
 
 db.create_all()
 
