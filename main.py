82c0ac1fe01186edddc4a7b00750097b6ba40123
mati <matizieba@gmail.com>
2022-04-04 10:55:20 +0200

    place edit

--- main.py
+++ main.py
@@ -53,8 +53,8 @@ class CofeHauses(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     user_name = db.Column(db.String(250), db.ForeignKey('users.name'))
 
-    user_numb = relationship("Users", foreign_keys = user_id)
-    user_nam = relationship("Users", foreign_keys = user_name)
+    user_numb = relationship("Users", foreign_keys = [user_id])
+    user_nam = relationship("Users", foreign_keys = [user_name])
 
 db.create_all()
 
